1f81fa83e131625ee947d45cabd6906fef88d797
Vinh <vinhbuith32412000@gmail.com>
2025-07-20 16:57:09 +0700

    improve views

--- app/helpers/payload_helper.rb
+++ app/helpers/payload_helper.rb
@@ -11,11 +11,13 @@ module PayloadHelper
   end
 
   def render_array_attribute(attribute)
-    safe_join(
-      attribute.map do |val|
-        content_tag(:span, val, class: "badge bg-info text-white me-1")
-      end
-    )
+    content_tag(:div, class: "mt-1") do
+      safe_join(
+        attribute.map do |val|
+          content_tag(:span, val, class: "badge bg-info text-white me-1")
+        end
+      )
+    end
   end
 
   def render_boolean_attribute(attribute)
